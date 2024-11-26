ad276344647ce0f6ba5d7114b015c9cbaefe7890
Ting97ok <89396424+Ting97ok@users.noreply.github.com>
2024-11-26 17:33:24 +0900

    [Fix] 뭐가 문제지

--- next.config.ts
+++ next.config.ts
@@ -1,152 +1,142 @@
-// next.config.ts
-import type { NextConfig } from 'next';
-import withPWAInit from '@ducanh2912/next-pwa';
-
-const withPWA = withPWAInit({
+/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
-  workboxOptions: {
-    skipWaiting: true,
-    disableDevLogs: true,
-    runtimeCaching: [
-      {
-        urlPattern: /^https:\/\/fonts\.(?:gstatic|googleapis)\.com\/.*/i,
-        handler: 'CacheFirst',
-        options: {
-          cacheName: 'google-fonts',
-          expiration: {
-            maxEntries: 4,
-            maxAgeSeconds: 365 * 24 * 60 * 60
-          }
-        }
-      },
-      {
-        urlPattern: /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
-        handler: 'StaleWhileRevalidate',
-        options: {
-          cacheName: 'static-font-assets',
-          expiration: {
-            maxEntries: 4,
-            maxAgeSeconds: 7 * 24 * 60 * 60
-          }
-        }
-      },
-      {
-        urlPattern: /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
-        handler: 'StaleWhileRevalidate',
-        options: {
-          cacheName: 'static-image-assets',
-          expiration: {
-            maxEntries: 64,
-            maxAgeSeconds: 24 * 60 * 60
-          }
-        }
-      },
-      {
-        urlPattern: /\/_next\/image\?url=.+$/i,
-        handler: 'StaleWhileRevalidate',
-        options: {
-          cacheName: 'next-image',
-          expiration: {
-            maxEntries: 64,
-            maxAgeSeconds: 24 * 60 * 60
-          }
-        }
-      },
-      {
-        urlPattern: /\.(?:mp3|wav|ogg)$/i,
-        handler: 'CacheFirst',
-        options: {
-          rangeRequests: true,
-          cacheName: 'static-audio-assets',
-          expiration: {
-            maxEntries: 32,
-            maxAgeSeconds: 24 * 60 * 60
-          }
-        }
-      },
-      {
-        urlPattern: /\.(?:js)$/i,
-        handler: 'StaleWhileRevalidate',
-        options: {
-          cacheName: 'static-js-assets',
-          expiration: {
-            maxEntries: 32,
-            maxAgeSeconds: 24 * 60 * 60
-          }
-        }
-      },
-      {
-        urlPattern: /\.(?:css|less)$/i,
-        handler: 'StaleWhileRevalidate',
-        options: {
-          cacheName: 'static-style-assets',
-          expiration: {
-            maxEntries: 32,
-            maxAgeSeconds: 24 * 60 * 60
-          }
-        }
-      },
-      {
-        urlPattern: /\/_next\/data\/.+\/.+\.json$/i,
-        handler: 'StaleWhileRevalidate',
-        options: {
-          cacheName: 'next-data',
-          expiration: {
-            maxEntries: 32,
-            maxAgeSeconds: 24 * 60 * 60
-          }
-        }
-      },
-      {
-        urlPattern: /\.(?:json|xml|csv)$/i,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'static-data-assets',
-          expiration: {
-            maxEntries: 32,
-            maxAgeSeconds: 24 * 60 * 60
-          }
-        }
-      },
-      {
-        urlPattern: ({ url }: { url: URL }) => {
-          const isSameOrigin = self.origin === url.origin;
-          if (!isSameOrigin) return false;
-          const pathname = url.pathname;
-          return !pathname.startsWith('/api/');
-        },
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'others',
-          expiration: {
-            maxEntries: 32,
-            maxAgeSeconds: 24 * 60 * 60
-          },
-          networkTimeoutSeconds: 10
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/fonts\.(?:gstatic|googleapis)\.com\/.*/i,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'google-fonts',
+        expiration: {
+          maxEntries: 4,
+          maxAgeSeconds: 365 * 24 * 60 * 60   // 1 year
         }
       }
-    ]
-  },
-  cacheOnFrontEndNav: true,
-  reloadOnOnline: true,
-  disable: process.env.NODE_ENV === 'development',
-});
+    },
+    {
+      urlPattern: /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'static-font-assets',
+        expiration: {
+          maxEntries: 4,
+          maxAgeSeconds: 7 * 24 * 60 * 60    // 1 week
+        }
+      }
+    },
+    {
+      urlPattern: /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'static-image-assets',
+        expiration: {
+          maxEntries: 64,
+          maxAgeSeconds: 24 * 60 * 60    // 24 hours
+        }
+      }
+    },
+    {
+      urlPattern: /\/_next\/image\?url=.+$/i,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'next-image',
+        expiration: {
+          maxEntries: 64,
+          maxAgeSeconds: 24 * 60 * 60    // 24 hours
+        }
+      }
+    },
+    {
+      urlPattern: /\.(?:mp3|wav|ogg)$/i,
+      handler: 'CacheFirst',
+      options: {
+        rangeRequests: true,
+        cacheName: 'static-audio-assets',
+        expiration: {
+          maxEntries: 32,
+          maxAgeSeconds: 24 * 60 * 60    // 24 hours
+        }
+      }
+    },
+    {
+      urlPattern: /\.(?:js)$/i,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'static-js-assets',
+        expiration: {
+          maxEntries: 32,
+          maxAgeSeconds: 24 * 60 * 60    // 24 hours
+        }
+      }
+    },
+    {
+      urlPattern: /\.(?:css|less)$/i,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'static-style-assets',
+        expiration: {
+          maxEntries: 32,
+          maxAgeSeconds: 24 * 60 * 60    // 24 hours
+        }
+      }
+    },
+    {
+      urlPattern: /\/_next\/data\/.+\/.+\.json$/i,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'next-data',
+        expiration: {
+          maxEntries: 32,
+          maxAgeSeconds: 24 * 60 * 60    // 24 hours
+        }
+      }
+    },
+    {
+      urlPattern: /\.(?:json|xml|csv)$/i,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'static-data-assets',
+        expiration: {
+          maxEntries: 32,
+          maxAgeSeconds: 24 * 60 * 60    // 24 hours
+        }
+      }
+    },
+    {
+      urlPattern: ({ url }: { url: URL }) => {
+        const isSameOrigin = self.origin === url.origin;
+        if (!isSameOrigin) return false;
+        const pathname = url.pathname;
+        return !pathname.startsWith('/api/');
+      },
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'others',
+        expiration: {
+          maxEntries: 32,
+          maxAgeSeconds: 24 * 60 * 60    // 24 hours
+        },
+        networkTimeoutSeconds: 10
+      }
+    }
+  ]
+})
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    // 다른 환경 변수들...
   },
-  swcMinify: true,
-  poweredByHeader: false,
-  reactStrictMode: true,
-  compress: true,
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
+  // Vercel 배포 시 환경 변수 파일 포함
+  experimental: {
+    outputFileTracingIncludes: {
+      '/**/*.env.production': true,
+    },
   }
-};
+// your existing next config
+}
 
-export default withPWA(nextConfig);
+module.exports = withPWA(nextConfig)
